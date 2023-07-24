5ef335d7b3ded92758c0471a9eea69597bfc0541
Greg Doyle <83316865+18gdoyle@users.noreply.github.com>
2023-07-23 21:27:16 -0400

    timeline cards expand

---
+++ greg-doyles-portfolio-site/app/components/TimeLineCard.tsx
@@ -0,0 +1,71 @@
+import './TimeLineCard.css';
+import './TimeLineCard';
+import React from 'react'
+
+export default function Home() {
+    const [maximizedContent, setMaximizedContent] = React.useState(false);
+
+    function enterHover() {
+        setMaximizedContent(true);
+    }
+
+    function exitHover() {
+        setMaximizedContent(false);
+    }
+
+    return (
+      <main>
+        <div className="card" onMouseEnter={enterHover} onMouseLeave={exitHover}>
+            <div className="content">
+                {maximizedContent ? 
+                    <div>
+                        <div className="flexItem">
+                            <p className="para">
+                                2014-2018
+                            </p>
+                        </div>
+                        <div className='flexItem'>
+                            <p className='heading'>Downingtown STEM Academy</p>
+                        </div>
+                        <div className="flexItem">
+                            <p className="para">
+                                <strong>GPA:</strong> 3.8
+                            </p>
+                        </div>
+                        <div className="flexItem">
+                            <p className="para">
+                                <strong>Activities and Accomplishments:</strong>
+                                <li>
+                                    Ski and Snowboard Club
+                                </li>
+                                <li>
+                                    National Honors Society
+                                </li>
+                            </p>
+                        </div>
+                        <div className='flexItem'>
+                            <hr className="rounded"></hr>
+                        </div>
+                        <div className='flexItem'>
+                            <p className="para">
+                                <a href="https://lilacrust.com/" target="_blank" className='hover-underline-animation'>The Project</a>
+                            </p>
+                            <p className="para">
+                                <a href="https://github.com/18gdoyle/lilac-rust-website" target="_blank" className='hover-underline-animation'>The Code</a>
+                            </p>
+                        </div>
+                    </div>
+                :
+                    <div>
+                        <div className="flexItem">
+                            <p className="para">
+                                2014-2018
+                            </p>
+                        </div>
+                    </div>
+                }
+            </div>
+        </div>
+      </main>
+    )
+  }
--- greg-doyles-portfolio-site/app/pages/ResumePage.tsx
+++ greg-doyles-portfolio-site/app/pages/ResumePage.tsx
@@ -1,12 +1,10 @@
 import './ResumePage.css'
+import TimeLineCard from '../components/TimeLineCard'
 
 export default function Home() {
     return (
         <main>
-            <div className='glassDiv'>
-                2014 - 2018
-                Downingtown STEM Acacdemy
-            </div>
+            <TimeLineCard />
         </main>
     )
   }
