f4a8b3860751f349a592466dc0a54dd746f10f71
Greg Doyle <83316865+18gdoyle@users.noreply.github.com>
2023-07-24 20:47:56 -0400

    gave time line card props

--- greg-doyles-portfolio-site/app/components/TimeLineCard.tsx
+++ greg-doyles-portfolio-site/app/components/TimeLineCard.tsx
@@ -2,7 +2,7 @@ import './TimeLineCard.css';
 import './TimeLineCard';
 import React from 'react'
 
-export default function Home() {
+export default function Home(props: any) {
     const [maximizedContent, setMaximizedContent] = React.useState(false);
 
     function enterHover() {
@@ -14,44 +14,30 @@ export default function Home() {
     }
 
     return (
-      <main className="timeLineCard" onMouseEnter={enterHover} onMouseLeave={exitHover}>
+      <main className={`timeLineCard ${props.color}`} onMouseEnter={enterHover} onMouseLeave={exitHover}>
         <div>
             <div className="content">
                 {maximizedContent ? 
                     <div>
                         <div className="flexItem">
                             <p className="para">
-                                2014-2018
+                                {props.date}
                             </p>
                         </div>
                         <div className='flexItem'>
-                            <p className='heading'>Downingtown STEM Academy</p>
+                            <p className='heading'>{props.title}</p>
+                        </div>
+                        <div className="flexItem">
+                            <p className="subtitle">
+                                {props.subtitle}
+                            </p>
                         </div>
                         <div className="flexItem">
                             <p className="para">
-                                <strong>GPA:</strong> 3.8
-                            </p>
-                        </div>
-                        <div className="flexItem">
-                            <p className="para">
-                                <strong>Activities and Accomplishments:</strong>
-                                <li>
-                                    Ski and Snowboard Club
-                                </li>
-                                <li>
-                                    National Honors Society
-                                </li>
-                            </p>
-                        </div>
-                        <div className='flexItem'>
-                            <hr className="rounded"></hr>
-                        </div>
-                        <div className='flexItem'>
-                            <p className="para">
-                                <a href="https://lilacrust.com/" target="_blank" className='hover-underline-animation'>The Project</a>
-                            </p>
-                            <p className="para">
-                                <a href="https://github.com/18gdoyle/lilac-rust-website" target="_blank" className='hover-underline-animation'>The Code</a>
+                                <strong>{props.descriptionTitle}</strong>
+                                <div>
+                                    {props.description}
+                                </div>
                             </p>
                         </div>
                     </div>
@@ -59,7 +45,7 @@ export default function Home() {
                     <div>
                         <div className="flexItem">
                             <p className="para">
-                                2014-2018
+                                {props.date}
                             </p>
                         </div>
                     </div>
--- greg-doyles-portfolio-site/app/pages/ResumePage.tsx
+++ greg-doyles-portfolio-site/app/pages/ResumePage.tsx
@@ -5,15 +5,54 @@ export default function Home() {
     return (
         <main className='timeLineContainer'>
             <div className='topOfTimeLine'>
-                <TimeLineCard />
-                <TimeLineCard />
+                <TimeLineCard 
+                    date="2014-2018"
+                    title="Downingtown STEM Academy"
+                    subtitle="High School Diploma - 3.8 GPA"
+                    descriptionTitle="Activities and Accomplishments"
+                    description={
+                        <div>
+                            <li>Ski and Snowboard Club</li>
+                            <li>National Honors Society</li>
+                        </div>
+                    }
+                    color="red"
+                />
+                <TimeLineCard 
+                    date="2021"
+                    title="Bruno Solutions"
+                    subtitle="Full Stack Developer"
+                    description={
+                        <div>
+                            <li>Developed a full-stack web application using the Angular framework for the front-end.</li>
+                            <li>Utilized AWS API Gateway, AWS Lambda (node.js and JavaScript), RDS Aurora (PostgreSQL), AWS S3, Google Firebase for user authentication, AWS CloudFront, AWS Route 53, and AWS CloudWatch for the backend.</li>
+                            <li>Gained experience with agile development, taking part in stand-ups every morning, and using GitHub for code reviews and merges.</li>
+                        </div>
+                    }
+                />
+                <TimeLineCard 
+                    date="2018-2022"
+                    title="University of Pittsburgh"
+                    subtitle="Bachelor of Science in Computer Science - 3.5 GPA"
+                    descriptionTitle="Notable Coursework"
+                    description={
+                        <div>
+                            <li>CS 1502: Formal Methods in Computer Science</li>
+                            <li>CS 1501: Algorithm Implementation</li>
+                            <li>CS 0449: Introduction to Systems Software</li>
+                            <li>CS 0447: Computer Organization and Assembly Language</li>
+                            <li>CS 0445: Discrete Structures</li>
+                        </div>
+                    }
+                />
             </div>
             <div className='timeLineLine'>
-                <hr></hr>
+                <div>2014</div>
+                <hr className="timeLineHR"></hr>
+                <div>Present</div>
             </div>
             <div className='bottomOfTimeLine'>
                 <div className="marginDiv"></div>
-                <TimeLineCard />
                 <TimeLineCard />
             </div>
         </main>
