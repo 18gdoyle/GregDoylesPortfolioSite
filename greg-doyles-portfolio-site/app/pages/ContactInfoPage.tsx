c9bee248d49ab2c22e8f7f5d3500a88907434fc9
Greg Doyle <83316865+18gdoyle@users.noreply.github.com>
2024-04-19 16:45:50 -0400

    updated contact info

--- greg-doyles-portfolio-site/app/pages/ContactInfoPage.tsx
+++ greg-doyles-portfolio-site/app/pages/ContactInfoPage.tsx
@@ -15,10 +15,10 @@ export default function Home() {
                 <h6 className="primary-text">Greg Doyle</h6>
                 <h6 className="secondary-text">Software Engineer</h6>
                 <br />
-                <h6 className="secondary-text"><FontAwesomeIcon className='icon' icon={faPhone} /> <a href='tel:610-233-8733' className="inlineLink hoverUnderlineAnimation contactCardColors"><strong>610-233-8733</strong></a></h6>
-                <h6 className="secondary-text"><FontAwesomeIcon className='icon' icon={faEnvelope} /> <a href='mailto:18gdoyle@gmail.com' className="inlineLink hoverUnderlineAnimation contactCardColors"><strong>18gdoyle@gmail.com</strong></a></h6>
-                <h6 className="secondary-text"><FontAwesomeIcon className='icon' icon={faLinkedinIn} /> <a href='https://www.linkedin.com/in/gregoryvincentdoyle/' className="inlineLink hoverUnderlineAnimation contactCardColors" target='_blank'><strong>LinkedIn Profile</strong></a></h6>
-                <h6 className="secondary-text"><FontAwesomeIcon className='icon' icon={faUpwork} /> <a href='https://www.upwork.com/freelancers/~011193481588fc4ca2' className="inlineLink hoverUnderlineAnimation contactCardColors" target='_blank'><strong>Upwork Profile</strong></a></h6>
+                <h6 className="secondary-text"><FontAwesomeIcon className='icon contactInfoIcon' icon={faPhone} /> <a href='tel:610-233-8733' className="inlineLink hoverUnderlineAnimation contactCardColors"><strong>610-233-8733</strong></a></h6>
+                <h6 className="secondary-text"><FontAwesomeIcon className='icon contactInfoIcon' icon={faEnvelope} /> <a href='mailto:18gdoyle@gmail.com' className="inlineLink hoverUnderlineAnimation contactCardColors"><strong>18gdoyle@gmail.com</strong></a></h6>
+                <h6 className="secondary-text"><FontAwesomeIcon className='icon contactInfoIcon' icon={faLinkedinIn} /> <a href='https://www.linkedin.com/in/gregoryvincentdoyle/' className="inlineLink hoverUnderlineAnimation contactCardColors" target='_blank'><strong>LinkedIn Profile</strong></a></h6>
+                <h6 className="secondary-text"><FontAwesomeIcon className='icon contactInfoIcon' icon={faUpwork} /> <a href='https://www.upwork.com/freelancers/~011193481588fc4ca2' className="inlineLink hoverUnderlineAnimation contactCardColors" target='_blank'><strong>Upwork Profile</strong></a></h6>
             </div>
     </main>
   )
